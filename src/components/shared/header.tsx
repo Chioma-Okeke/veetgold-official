0fa688245d76d4f21fd4fe33eb5d827dac09a3c4
Okeke Chioma Princess <okekegeraldenne@gmail.com>
2025-07-11 11:55:08 +0100

    feat: updated the logo on the header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -17,7 +17,9 @@ function Header() {
         <header className='z-50 w-full absolute top-0 left-0 p-4 lg:px-[100px] lg:py-6 lg:bg-black/33 text-white'>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center lg:gap-32'>
-                    <Logo variant={(pathname === "/about-us" && width < 1024) ? "colored" : "default"} />
+                    <Link href="/">
+                        <Logo variant={(pathname === "/about-us" && width && width < 1024) ? "colored" : "default"} />
+                    </Link>
                     <nav className='hidden lg:block'>
                         <ul className='flex items-center gap-8'>
                             {HEADER_URLS.map((item) => {
